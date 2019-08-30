57fb430882212f9075abb7f45d676ebe78ac1847
Selchuk Karakus <selchuck.karakus@gmail.com>
2019-08-30 17:50:49 +0100

    add cardlist component

---
+++ src/CardList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = () => {
+  return (
+
+    );
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card'
+import CardList from './CardList';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(<CardList />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
