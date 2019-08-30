f6c1745b6037a41b8e86bd38a6c9689a148342d0
Selchuk Karakus <selchuck.karakus@gmail.com>
2019-08-30 18:47:20 +0100

    fix img issue

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='tc bg-light-green dib br2 pa3 ma2 grow'>
-      <img src={'https://robohash.org/${id}?250x250'} alt='profile of a robot'/>
+      <img alt='profile of a robot' src={`https://robohash.org/${id}?250x250`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card'
+// import Card from './Card'
 import CardList from './CardList';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots'
 
-ReactDOM.render(<CardList />, document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
